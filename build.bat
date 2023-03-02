a025a530c980a4e03d01fb505cef9f4aebefcc1b
ecourse <smkang@codenuri.co.kr>
2023-03-02 15:21:51 +0900

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,21 @@
+cls
+
+@echo [101mcl %* /permissive- /std:c++latest /utf-8 /nologo /EHsc /link /out:vout.exe [0m
+@cl  %* /permissive- /std:c++latest /utf-8 /IE:\1_ONLINE\COMMON /nologo /EHsc /link /out:vout.exe
+
+@IF %ERRORLEVEL%  EQU 0 (
+	echo [37;4m[ cl compiling success !! run vout.exe ][0m
+	vout.exe
+)ELSE (
+	echo [37;4m[ Error !!, cl fail to compile. ][0m
+)
+
+@echo [101mg++ %* -std=c++23 -o gout.exe [0m
+@g++ %* -std=c++23 -IE:\1_ONLINE\COMMON -o gout.exe
+
+@IF %ERRORLEVEL% EQU 0 (
+	echo [37;4m[ g++ compiling success !! run gout.exe ][0m
+  	gout.exe
+) ELSE (
+	echo [37;4m[ Error !!, g++ fail to compile. ][0m
+)
---
+++ pt.bat
@@ -0,0 +1,3 @@
+set prompt=$G 
+
+"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
